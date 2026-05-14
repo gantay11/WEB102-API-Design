42fee7372b7327251628bb6c96c08fbd15f46be5
gantay11 <sanskargurung412@gmail.com>
2026-05-14 14:13:57 +0600

    p5

--- partical 4/server/src/app.js
+++ partical 4/server/src/app.js
@@ -2,16 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const path = require('path');
-const fs = require('fs');
-
 
 // Import routes
 const userRoutes = require('./routes/users');
 const videoRoutes = require('./routes/videos');
 const commentRoutes = require('./routes/comments');
-
-
 
 const app = express();
 
@@ -25,15 +20,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/api/users', userRoutes);
 app.use('/api/videos', videoRoutes);
 app.use('/api/comments', commentRoutes);
-// Create uploads directory if it doesn't exist
-const uploadsDir = path.join(__dirname, 'uploads');
-if (!fs.existsSync(uploadsDir)) {
-  fs.mkdirSync(uploadsDir, { recursive: true });
-}
-
-// Serve files from uploads directory
-app.use('/uploads', express.static(uploadsDir));
-
 
 // Error handling middleware
 app.use((req, res, next) => {
